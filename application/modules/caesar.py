6f0388f79c0d65339c8e982be1174467dee6bf4b
theodoremoreland <theodoremoreland@yahoo.com>
2024-10-17 19:53:54 -0500

    refactor

--- application/modules/caesar.py
+++ application/modules/caesar.py
@@ -1,33 +1,19 @@
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
+
 def alphabet_position(character):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
     lower = character.lower()
 
-    return alphabet.index(lower)
-
-
-def rotate_string_13(text):
-    rotated = ""
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-    for char in text:
-        rotated_idx = (alphabet_position(char) + 13) % 26
-
-        if char.isupper():
-            rotated = rotated + alphabet[rotated_idx].upper()
-        else:
-            rotated = rotated + alphabet[rotated_idx]
-
-    return rotated
+    return ALPHABET.index(lower)
 
 
 def rotate_character(char, rot):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
     rotated_idx = (alphabet_position(char) + rot) % 26
 
     if char.isupper():
-        return alphabet[rotated_idx].upper()
+        return ALPHABET[rotated_idx].upper()
     else:
-        return alphabet[rotated_idx]
+        return ALPHABET[rotated_idx]
 
 
 def rotate_string(text, rot):
