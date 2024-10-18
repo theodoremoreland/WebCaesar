88cce5b4cd7d75eb1040ac16bed36e3f82824494
theodoremoreland <theodoremoreland@yahoo.com>
2024-10-17 19:10:52 -0500

    renamed scripts > modules

--- main.py
+++ main.py
@@ -2,24 +2,25 @@
 from flask import Flask, request, render_template
 
 # Custom
-from scripts.caesar import rotate_string
+from modules.caesar import rotate_string
 
 app = Flask(__name__)
-app.config['DEBUG'] = False
+app.config["DEBUG"] = False
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html", text="")
 
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=["POST"])
 def return_encrypted_text():
     rot = int(request.form["rot"])
     text = request.form["text"]
     encrypted_text = rotate_string(text, rot)
+
     return render_template("index.html", text=encrypted_text)
 
-    
+
 if __name__ == "__main__":
     app.run()
