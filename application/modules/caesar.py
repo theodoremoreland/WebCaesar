2e84a08a58dd15f5681cd93d0e4c6920da1b2703
theodoremoreland <theodoremoreland@yahoo.com>
2024-10-21 23:19:55 -0500

    now attempts to decrypt in all supported languages

--- application/modules/caesar.py
+++ application/modules/caesar.py
@@ -1,24 +1,23 @@
 # First party
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 # Third party
 from spellchecker import SpellChecker
 
-ALPHABET = "abcdefghijklmnopqrstuvwxyz"
-
-# English - "en"
-# Spanish - "es"
-# French - "fr"
-# Portuguese - "pt"
-# German - "de"
-# Italian - "it"
-# Russian - "ru"
-# Arabic - "ar"
-# Basque - "eu"
-# Latvian - "lv"
-# Dutch - "nl"
-
-spell = SpellChecker(distance=1, language="en")
+ALPHABET: str = "abcdefghijklmnopqrstuvwxyz"
+LANGUAGES: List[Dict[str, str]] = [
+    {"name": "English", "code": "en"},
+    {"name": "Spanish", "code": "es"},
+    {"name": "French", "code": "fr"},
+    {"name": "Portuguese", "code": "pt"},
+    {"name": "German", "code": "de"},
+    {"name": "Italian", "code": "it"},
+    {"name": "Russian", "code": "ru"},
+    {"name": "Arabic", "code": "ar"},
+    {"name": "Basque", "code": "eu"},
+    {"name": "Latvian", "code": "lv"},
+    {"name": "Dutch", "code": "nl"},
+]
 
 
 def alphabet_position(character: str) -> int:
@@ -49,16 +48,27 @@ def rotate_string(text: str, rot: int) -> str:
 
 
 def decrypt(text: str) -> Dict[str, Any]:
-    best_match = {"rot": 0, "result": "", "matches": 0}
+    best_match: Dict[str, Any] = {
+        "rot": 0,
+        "result": "",
+        "matches": 0,
+        "language": "",
+        "language_code": "",
+    }
 
-    for i in range(25):
-        rot_result = rotate_string(text, i)
-        words = rot_result.split()
-        known = spell.known(words)
+    for language in LANGUAGES:
+        spell = SpellChecker(distance=1, language=language["code"])
 
-        if len(known) > best_match["matches"]:
-            best_match["rot"] = i
-            best_match["result"] = rot_result
-            best_match["matches"] = len(known)
+        for i in range(25):
+            rot_result: str = rotate_string(text, i)
+            words: List[str] = rot_result.split()
+            known = spell.known(words)
+
+            if len(known) > best_match["matches"]:
+                best_match["rot"] = i
+                best_match["result"] = rot_result
+                best_match["matches"] = len(known)
+                best_match["language"] = language["name"]
+                best_match["language_code"] = language["code"]
 
     return best_match
