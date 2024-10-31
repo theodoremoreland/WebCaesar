b3be33b01491d59e37a0381a4e08694b8abf9ed3
theodoremoreland <theodoremoreland@yahoo.com>
2024-10-30 22:45:51 -0500

    completely replaced old alphabet data structures and algos: first draft

--- application/ui/src/modules/rotateString.ts
+++ application/ui/src/modules/rotateString.ts
@@ -1,9 +1,37 @@
-interface AlphabetPosition {
+export enum SupportedLanguage {
+	English = "English",
+	Spanish = "Spanish",
+	French = "French",
+	Portuguese = "Portuguese",
+	German = "German",
+	Italian = "Italian",
+	Russian = "Russian",
+	Basque = "Basque",
+	Latvian = "Latvian",
+	Dutch = "Dutch",
+}
+
+interface IndexToCharacters {
 	[key: number]: string[];
 }
 
-const ALPHABET_EN: string = "abcdefghijklmnopqrstuvwxyz";
-const ALPHABET_POSITIONS_EN: AlphabetPosition = {
+interface CharactersToIndex {
+	[key: string]: number;
+}
+
+const deriveCharactersToIndex = (indexToCharacters: IndexToCharacters) => {
+	const charactersToIndex: CharactersToIndex = {};
+
+	for (const [index, characters] of Object.entries(indexToCharacters)) {
+		for (const character of characters) {
+			charactersToIndex[character] = parseInt(index);
+		}
+	}
+
+	return charactersToIndex;
+};
+
+const INDEX_TO_CHARACTERS_EN: IndexToCharacters = {
 	0: ["a"],
 	1: ["b"],
 	2: ["c"],
@@ -31,13 +59,16 @@ const ALPHABET_POSITIONS_EN: AlphabetPosition = {
 	24: ["y"],
 	25: ["z"],
 };
-const ALPHABET_ES: string = "abcdefghijklmnñopqrstuvwxyz";
+const CHARACTERS_TO_INDEX_EN: CharactersToIndex = deriveCharactersToIndex(
+	INDEX_TO_CHARACTERS_EN
+);
+
 /**
  * The Spanish alphabet is similar to the English alphabet but includes one additional letter: "ñ". Here is the traditional Spanish alphabet in order:
  * a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z
  * Historically, "ch" and "ll" were also considered separate letters in the Spanish alphabet, but in 2010, the Royal Spanish Academy officially removed them as distinct letters, aligning the Spanish alphabet more closely with the standard Latin alphabet plus the "ñ".
  */
-const ALPHABET_POSITIONS_ES: AlphabetPosition = {
+const INDEX_TO_CHARACTERS_ES: IndexToCharacters = {
 	0: ["a"],
 	1: ["b"],
 	2: ["c"],
@@ -66,14 +97,16 @@ const ALPHABET_POSITIONS_ES: AlphabetPosition = {
 	25: ["y"],
 	26: ["z"],
 };
-const ALPHABET_FR: string = "abcdefghijklmnopqrstuvwxyzàâæçéèêëîïôœùûüÿ";
+const CHARACTERS_TO_INDEX_ES: CharactersToIndex = deriveCharactersToIndex(
+	INDEX_TO_CHARACTERS_ES
+);
 /**
  *
  * The French alphabet has the exact same letters and order as the English alphabet:
  * a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
  * While the alphabet is the same, French uses additional accents and special characters in writing, like é, è, ê, ë and ç (cédille), but these are not considered separate letters in the French alphabet.
  */
-const ALPHABET_POSITIONS_FR: AlphabetPosition = {
+const INDEX_TO_CHARACTERS_FR: IndexToCharacters = {
 	0: ["a", "à", "â"],
 	1: ["b"],
 	2: ["c", "ç"],
@@ -101,13 +134,15 @@ const ALPHABET_POSITIONS_FR: AlphabetPosition = {
 	24: ["y", "ÿ"],
 	25: ["z"],
 };
-const ALPHABET_PT: string = "abcdefghijklmnopqrstuvwxyzàáâãçéêíóôõú";
+const CHARACTERS_TO_INDEX_FR: CharactersToIndex = deriveCharactersToIndex(
+	INDEX_TO_CHARACTERS_FR
+);
 /**
  * The Portuguese alphabet has the same 26 letters as the English alphabet. Here is the Portuguese alphabet in order:
  * a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
  * However, Portuguese also uses various diacritical marks (like accents) on vowels and the ç (c-cedilla), but these do not add extra letters to the alphabet.
  */
-const ALPHABET_POSITIONS_PT: AlphabetPosition = {
+const INDEX_TO_CHARACTERS_PT: IndexToCharacters = {
 	0: ["a", "à", "á", "â", "ã"],
 	1: ["b"],
 	2: ["c", "ç"],
@@ -135,8 +170,9 @@ const ALPHABET_POSITIONS_PT: AlphabetPosition = {
 	24: ["y"],
 	25: ["z"],
 };
-
-const ALPHABET_DE: string = "abcdefghijklmnopqrstuvwxyzäöüß";
+const CHARACTERS_TO_INDEX_PT: CharactersToIndex = deriveCharactersToIndex(
+	INDEX_TO_CHARACTERS_PT
+);
 /**
  * The German alphabet has the same 26 letters as the English alphabet. Here it is in order:
  * a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
@@ -145,7 +181,7 @@ const ALPHABET_DE: string = "abcdefghijklmnopqrstuvwxyzäöüß";
  * The Eszett (ß), also called the "sharp S," which is used in place of a double "s" in certain cases
  * However, ä, ö, ü, and ß are not considered separate letters of the alphabet but variations of the existing letters.
  */
-const ALPHABET_POSITIONS_DE: AlphabetPosition = {
+const INDEX_TO_CHARACTERS_DE: IndexToCharacters = {
 	0: ["a"],
 	1: ["b"],
 	2: ["c"],
@@ -173,13 +209,15 @@ const ALPHABET_POSITIONS_DE: AlphabetPosition = {
 	24: ["y"],
 	25: ["z"],
 };
-const ALPHABET_IT: string = "abcdefghijklmnopqrstuvwxyzàèéìíîòóùú";
+const CHARACTERS_TO_INDEX_DE: CharactersToIndex = deriveCharactersToIndex(
+	INDEX_TO_CHARACTERS_DE
+);
 /**
  * The Italian alphabet traditionally has only 21 letters:
  * a, b, c, d, e, f, g, h, i, l, m, n, o, p, q, r, s, t, u, v, z
  * The letters j, k, w, x, and y are generally excluded from the traditional Italian alphabet because they are not native to Italian words. However, these letters do appear in borrowed foreign words, names, and scientific terms, so they are recognized and used in modern Italian when needed.
  */
-const ALPHABET_POSITIONS_IT: AlphabetPosition = {
+const INDEX_TO_CHARACTERS_IT: IndexToCharacters = {
 	0: ["a"],
 	1: ["b"],
 	2: ["c"],
@@ -202,13 +240,15 @@ const ALPHABET_POSITIONS_IT: AlphabetPosition = {
 	19: ["v"],
 	20: ["z"],
 };
-const ALPHABET_RU: string = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+const CHARACTERS_TO_INDEX_IT: CharactersToIndex = deriveCharactersToIndex(
+	INDEX_TO_CHARACTERS_IT
+);
 /**
  * The Russian alphabet uses the Cyrillic script, which has 33 letters. Here is the Russian alphabet in order:
  * А, Б, В, Г, Д, Е, Ё, Ж, З, И, Й, К, Л, М, Н, О, П, Р, С, Т, У, Ф, Х, Ц, Ч, Ш, Щ, Ъ, Ы, Ь, Э, Ю, Я
  * Some letters may look similar to those in the Latin alphabet, but they often represent different sounds. For example, "В" sounds like the English "V" and "Н" sounds like the English "N".
  */
-const ALPHABET_POSITIONS_RU: AlphabetPosition = {
+const INDEX_TO_CHARACTERS_RU: IndexToCharacters = {
 	0: ["а"],
 	1: ["б"],
 	2: ["в"],
@@ -242,13 +282,15 @@ const ALPHABET_POSITIONS_RU: AlphabetPosition = {
 	30: ["ю"],
 	31: ["я"],
 };
-const ALPHABET_EU: string = "abcdefghijklmnopqrstuvwxyzñ";
+const CHARACTERS_TO_INDEX_RU: CharactersToIndex = deriveCharactersToIndex(
+	INDEX_TO_CHARACTERS_RU
+);
 /**
  * The Basque alphabet uses the same 26 letters as the English alphabet, in the same order:
  * a, b, c, d, e, f, g, h, i, j, k, l, m, n, ñ, o, p, q, r, s, t, u, v, w, x, y, z
  * While "ñ" is not a separate letter in most Latin alphabets, it is commonly included in Basque. Additionally, Basque has some unique pronunciation rules and uses combinations like "tx," "ts," and "tz" to represent sounds that are distinct in Basque phonetics.
  */
-const ALPHABET_POSITIONS_EU: AlphabetPosition = {
+const INDEX_TO_CHARACTERS_EU: IndexToCharacters = {
 	0: ["a"],
 	1: ["b"],
 	2: ["c"],
@@ -276,13 +318,15 @@ const ALPHABET_POSITIONS_EU: AlphabetPosition = {
 	24: ["y"],
 	25: ["z"],
 };
-const ALPHABET_LV: string = "aābcčdeēfgģhiījkķlļmnņoprsštuūvzž";
+const CHARACTERS_TO_INDEX_EU: CharactersToIndex = deriveCharactersToIndex(
+	INDEX_TO_CHARACTERS_EU
+);
 /**
  * The Latvian alphabet has 33 letters and is based on the Latin alphabet with some additional letters that include diacritical marks. Here is the Latvian alphabet in order:
  * a, ā, b, c, č, d, e, ē, f, g, ģ, h, i, ī, j, k, ķ, l, ļ, m, n, ņ, o, p, r, s, š, t, u, ū, v, z, ž
  * Notably, the letters q, w, x, and y from the English alphabet are not used in standard Latvian and appear only in foreign words or names.
  */
-const ALPHABET_POSITIONS_LV: AlphabetPosition = {
+const INDEX_TO_CHARACTERS_LV: IndexToCharacters = {
 	0: ["a"],
 	1: ["ā"],
 	2: ["b"],
@@ -316,13 +360,15 @@ const ALPHABET_POSITIONS_LV: AlphabetPosition = {
 	30: ["z"],
 	31: ["ž"],
 };
-const ALPHABET_NL: string = "abcdefghijklmnopqrstuvwxyz";
+const CHARACTERS_TO_INDEX_LV: CharactersToIndex = deriveCharactersToIndex(
+	INDEX_TO_CHARACTERS_LV
+);
 /**
  * The Dutch alphabet has the same 26 letters as the English alphabet, in the same order:
  * a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
  * Dutch also uses digraphs (combinations of two letters) such as "ij" and "oe" to represent specific sounds, but these are not considered separate letters in the alphabet.
  */
-const ALPHABET_POSITIONS_NL: AlphabetPosition = {
+const INDEX_TO_CHARACTERS_NL: IndexToCharacters = {
 	0: ["a"],
 	1: ["b"],
 	2: ["c"],
@@ -350,76 +396,126 @@ const ALPHABET_POSITIONS_NL: AlphabetPosition = {
 	24: ["y"],
 	25: ["z"],
 };
-export enum SupportedLanguage {
-	English = "English",
-	Spanish = "Spanish",
-	French = "French",
-	Portuguese = "Portuguese",
-	German = "German",
-	Italian = "Italian",
-	Russian = "Russian",
-	Basque = "Basque",
-	Latvian = "Latvian",
-	Dutch = "Dutch",
-}
+const CHARACTERS_TO_INDEX_NL: CharactersToIndex = deriveCharactersToIndex(
+	INDEX_TO_CHARACTERS_NL
+);
+
 export const supportedLanguages: {
 	[K in SupportedLanguage]: {
-		alphabet: string;
+		characterCount: number;
+		charactersToIndex: CharactersToIndex;
+		indexToCharacters: IndexToCharacters;
 		code: string;
 	};
 } = {
-	English: { alphabet: ALPHABET_EN, code: "en" },
-	Spanish: { alphabet: ALPHABET_ES, code: "es" },
-	French: { alphabet: ALPHABET_FR, code: "fr" },
-	Portuguese: { alphabet: ALPHABET_PT, code: "pt" },
-	German: { alphabet: ALPHABET_DE, code: "de" },
-	Italian: { alphabet: ALPHABET_IT, code: "it" },
-	Russian: { alphabet: ALPHABET_RU, code: "ru" },
-	Basque: { alphabet: ALPHABET_EU, code: "eu" },
-	Latvian: { alphabet: ALPHABET_LV, code: "lv" },
-	Dutch: { alphabet: ALPHABET_NL, code: "nl" },
+	English: {
+		characterCount: Object.keys(INDEX_TO_CHARACTERS_EN).length,
+		charactersToIndex: CHARACTERS_TO_INDEX_EN,
+		indexToCharacters: INDEX_TO_CHARACTERS_EN,
+		code: "en",
+	},
+	Spanish: {
+		characterCount: Object.keys(INDEX_TO_CHARACTERS_ES).length,
+		charactersToIndex: CHARACTERS_TO_INDEX_ES,
+		indexToCharacters: INDEX_TO_CHARACTERS_ES,
+		code: "es",
+	},
+	French: {
+		characterCount: Object.keys(INDEX_TO_CHARACTERS_FR).length,
+		charactersToIndex: CHARACTERS_TO_INDEX_FR,
+		indexToCharacters: INDEX_TO_CHARACTERS_FR,
+		code: "fr",
+	},
+	Portuguese: {
+		characterCount: Object.keys(INDEX_TO_CHARACTERS_PT).length,
+		charactersToIndex: CHARACTERS_TO_INDEX_PT,
+		indexToCharacters: INDEX_TO_CHARACTERS_PT,
+		code: "pt",
+	},
+	German: {
+		characterCount: Object.keys(INDEX_TO_CHARACTERS_DE).length,
+		charactersToIndex: CHARACTERS_TO_INDEX_DE,
+		indexToCharacters: INDEX_TO_CHARACTERS_DE,
+		code: "de",
+	},
+	Italian: {
+		characterCount: Object.keys(INDEX_TO_CHARACTERS_IT).length,
+		charactersToIndex: CHARACTERS_TO_INDEX_IT,
+		indexToCharacters: INDEX_TO_CHARACTERS_IT,
+		code: "it",
+	},
+	Russian: {
+		characterCount: Object.keys(INDEX_TO_CHARACTERS_RU).length,
+		charactersToIndex: CHARACTERS_TO_INDEX_RU,
+		indexToCharacters: INDEX_TO_CHARACTERS_RU,
+		code: "ru",
+	},
+	Basque: {
+		characterCount: Object.keys(INDEX_TO_CHARACTERS_EU).length,
+		charactersToIndex: CHARACTERS_TO_INDEX_EU,
+		indexToCharacters: INDEX_TO_CHARACTERS_EU,
+		code: "eu",
+	},
+	Latvian: {
+		characterCount: Object.keys(INDEX_TO_CHARACTERS_LV).length,
+		charactersToIndex: CHARACTERS_TO_INDEX_LV,
+		indexToCharacters: INDEX_TO_CHARACTERS_LV,
+		code: "lv",
+	},
+	Dutch: {
+		characterCount: Object.keys(INDEX_TO_CHARACTERS_NL).length,
+		charactersToIndex: CHARACTERS_TO_INDEX_NL,
+		indexToCharacters: INDEX_TO_CHARACTERS_NL,
+		code: "nl",
+	},
 };
 
-const findAlphabetPosition = (character: string, alphabet: string): number => {
+const findCharacterIndex = (
+	character: string,
+	language: SupportedLanguage
+): number => {
 	const lower: string = character.toLowerCase();
 
-	return alphabet.indexOf(lower);
+	return supportedLanguages[language].charactersToIndex[lower] ?? -1;
 };
 
 const rotateCharacter = (
 	char: string,
 	rot: number,
-	alphabet: string
+	language: SupportedLanguage
 ): string => {
-	const alphabetPosition: number = findAlphabetPosition(char, alphabet);
+	const characterIndex: number = findCharacterIndex(char, language);
 
-	if (alphabetPosition === -1) {
+	if (characterIndex === -1) {
 		throw new Error(
 			`Character ${char} is not a valid character for chosen language.`
 		);
 	}
 
-	const rotatedIndex: number = (alphabetPosition + rot) % alphabet.length;
+	const rotatedIndex: number =
+		(characterIndex + rot) % supportedLanguages[language].characterCount;
+	const rotatedCharacter: string =
+		supportedLanguages[language].indexToCharacters[rotatedIndex][0];
 
 	if (char.toUpperCase() === char) {
-		return alphabet[rotatedIndex].toUpperCase();
+		return rotatedCharacter.toUpperCase();
 	}
 
-	return alphabet[rotatedIndex];
+	return rotatedCharacter;
 };
 
 export default (
 	text: string,
 	rot: number,
-	alphabet: string = ALPHABET_EN
+	language: SupportedLanguage = SupportedLanguage.English
 ): string => {
 	let rotated: string = "";
 
 	for (const char of text) {
-		if (alphabet.includes(char.toLowerCase())) {
-			rotated = rotated + rotateCharacter(char, rot, alphabet);
+		if (supportedLanguages[language].charactersToIndex[char.toLowerCase()]) {
+			rotated = rotated + rotateCharacter(char, rot, language);
 		} else {
-			rotated = rotated + char;
+			rotated += char;
 		}
 	}
 
