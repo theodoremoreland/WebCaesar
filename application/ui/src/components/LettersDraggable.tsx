1a7ca0f1a99e4e36003a85df0ac42266cd68efc0
theodoremoreland <theodoremoreland@yahoo.com>
2024-11-30 15:03:00 -0600

    progress on letters draggable component'

--- application/ui/src/components/LettersDraggable.tsx
+++ application/ui/src/components/LettersDraggable.tsx
@@ -1,19 +1,71 @@
-import { ReactElement } from "react";
+// React
+import { ReactElement, useState } from "react";
+
+// Custom
 import { SupportedLanguage, supportedLanguages } from "../modules/rotateString";
 
 // Styles
 import "./LettersDraggable.css";
+
+interface CharacterPositions {
+    original: {
+        [character: string]: {
+            x: number;
+            y: number;
+            index: number;
+        };
+    };
+    rotated: {
+        [character: string]: {
+            x: number;
+            y: number;
+            index: number;
+        };
+    };
+}
 
 interface Props {
     originalLanguage: SupportedLanguage;
     rotatedLanguage: SupportedLanguage;
 }
 
+function onMouseUp() {
+    document.removeEventListener("mousemove", onMouseMove);
+}
+
+function onMouseMove(event: MouseEvent) {
+    document.addEventListener("mouseup", onMouseUp);
+    const ol = document.getElementById(
+        "character-list-original"
+    ) as HTMLOListElement;
+
+    const y = event.clientY;
+
+    console.log("onMouseMove", y);
+
+    ol.style.transition = "transform 0.2s ease";
+    ol.style.transform = `translateY(${y}px)`;
+}
+
+function onMouseDown(event: React.MouseEvent<HTMLOListElement, MouseEvent>) {
+    const ol = event.currentTarget;
+
+    console.log("onMouseDown", ol);
+
+    document.addEventListener("mousemove", onMouseMove);
+}
+
 const LettersDraggable = ({
     originalLanguage,
     rotatedLanguage,
 }: Props): ReactElement => {
-    const originalCharactersDoubled = [
+    const [characterPositions, setCharacterPositions] =
+        useState<CharacterPositions>({
+            original: {},
+            rotated: {},
+        });
+
+    const originalCharactersDoubled: string[] = [
         ...Object.values(
             supportedLanguages[originalLanguage].indexToCharacters
         ),
@@ -21,31 +73,24 @@ const LettersDraggable = ({
             supportedLanguages[originalLanguage].indexToCharacters
         ),
     ];
-    const rotatedCharactersDoubled = [
+    const rotatedCharactersDoubled: string[] = [
         ...Object.values(supportedLanguages[rotatedLanguage].indexToCharacters),
         ...Object.values(supportedLanguages[rotatedLanguage].indexToCharacters),
     ];
+
     return (
-        <div className="LettersDraggable rot-wheel-container">
-            <ol>
+        <section className="LettersDraggable">
+            <ol id="character-list-original" onMouseDown={onMouseDown}>
                 {originalCharactersDoubled.map((character, index) => {
-                    return (
-                        <li key={index + character} draggable={true}>
-                            {character}
-                        </li>
-                    );
+                    return <li key={index + character}>{character}</li>;
                 })}
             </ol>
-            <ol>
+            <ol id="character-list-rotated">
                 {rotatedCharactersDoubled.map((character, index) => {
-                    return (
-                        <li key={index + character} draggable={true}>
-                            {character}
-                        </li>
-                    );
+                    return <li key={index + character}>{character}</li>;
                 })}
             </ol>
-        </div>
+        </section>
     );
 };
 
