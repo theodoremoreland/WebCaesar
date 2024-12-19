e798a0ffa97835fee171d30298d31cf73e5f0a64
theodoremoreland <theodoremoreland@yahoo.com>
2024-12-19 07:00:22 -0600

    refactor: moved some event handlers to controller

--- application/ui/src/components/LettersDraggable/LettersDraggable.controller.ts
+++ application/ui/src/components/LettersDraggable/LettersDraggable.controller.ts
@@ -21,3 +21,98 @@ export const quadruple = (array: string[]): string[] => {
 export const get25Percent = (value: number): number => {
     return value * 0.25;
 };
+
+// export const getCenterLetters = () => {
+//         // const elements = originalOlRef.current.getElementsByTagName("li");
+
+//         // console.log(
+//         //     "onMouseUp",
+//         //     [...elements].map((e) => {
+//         //         return {
+//         //             letter: e.innerText,
+//         //             position: e.getBoundingClientRect().top + window.scrollY,
+//         //         };
+//         //     }),
+//         //     window.innerHeight,
+//         //     `Center: ${window.innerHeight / 2}`
+//         // );
+// }
+
+export const onWheelMove = (
+    event: React.WheelEvent<HTMLOListElement>,
+    {
+        olRef,
+        startingMousePositionRef,
+        startingOlTop,
+    }: {
+        olRef: React.MutableRefObject<HTMLOListElement | null>;
+        startingMousePositionRef: React.MutableRefObject<number | null>;
+        startingOlTop: React.MutableRefObject<number | null>;
+    }
+) => {
+    event.preventDefault();
+
+    if (
+        olRef.current === null ||
+        startingMousePositionRef.current === null ||
+        startingOlTop.current === null
+    ) {
+        return;
+    }
+
+    const deltaY: number =
+        event.deltaY > 10 ? 10 : event.deltaY < -10 ? -10 : event.deltaY;
+
+    // Get the bounding rectangles of the child and parent elements
+    const childRect: DOMRect = olRef.current.getBoundingClientRect();
+
+    const newTop: number =
+        Number(olRef.current.style.top.replace("px", "")) + deltaY;
+    const isWithinResetThresholdScrollingDown: boolean =
+        newTop - get25Percent(childRect.height) >= 10;
+    const isWithinResetThresholdScrollingUp: boolean =
+        newTop + get25Percent(childRect.height) <= 10;
+
+    olRef.current.style.top = `${newTop}px`;
+
+    if (deltaY > 0 && isWithinResetThresholdScrollingDown) {
+        olRef.current.style.top = `${
+            newTop - get25Percent(childRect.height)
+        }px`;
+    } else if (deltaY < 0 && isWithinResetThresholdScrollingUp) {
+        olRef.current.style.top = `${
+            newTop + get25Percent(childRect.height)
+        }px`;
+    } else {
+        olRef.current.style.top = `${newTop}px`;
+    }
+};
+
+export const onMouseDown = (
+    event: React.MouseEvent<HTMLOListElement>,
+    {
+        olRef,
+        startingMousePositionRef,
+        startingOlTop,
+        onMouseMove,
+        onMouseUp,
+    }: {
+        olRef: React.MutableRefObject<HTMLOListElement | null>;
+        startingMousePositionRef: React.MutableRefObject<number | null>;
+        startingOlTop: React.MutableRefObject<number | null>;
+        onMouseMove: (event: MouseEvent) => void;
+        onMouseUp: () => void;
+    }
+) => {
+    if (!olRef.current) {
+        return;
+    }
+
+    startingOlTop.current = Number(
+        window.getComputedStyle(olRef.current).top.replace("px", "")
+    );
+    startingMousePositionRef.current = event.clientY;
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+};
--- application/ui/src/components/LettersDraggable/LettersDraggable.tsx
+++ application/ui/src/components/LettersDraggable/LettersDraggable.tsx
@@ -6,7 +6,13 @@ import {
     SupportedLanguage,
     supportedLanguages,
 } from "../../modules/rotateString";
-import { quadruple, get25Percent, fill } from "./LettersDraggable.controller";
+import {
+    quadruple,
+    get25Percent,
+    fill,
+    onMouseDown,
+    onWheelMove,
+} from "./LettersDraggable.controller";
 
 // Styles
 import "./LettersDraggable.css";
@@ -14,85 +20,6 @@ import "./LettersDraggable.css";
 interface Props {
     originalLanguage: SupportedLanguage;
     rotatedLanguage: SupportedLanguage;
-}
-
-function onWheelMove(
-    event: React.WheelEvent<HTMLOListElement>,
-    {
-        olRef,
-        startingMousePositionRef,
-        startingOlTop,
-    }: {
-        olRef: React.MutableRefObject<HTMLOListElement | null>;
-        startingMousePositionRef: React.MutableRefObject<number | null>;
-        startingOlTop: React.MutableRefObject<number | null>;
-    }
-) {
-    event.preventDefault();
-
-    if (
-        olRef.current === null ||
-        startingMousePositionRef.current === null ||
-        startingOlTop.current === null
-    ) {
-        return;
-    }
-
-    const deltaY: number =
-        event.deltaY > 10 ? 10 : event.deltaY < -10 ? -10 : event.deltaY;
-
-    // Get the bounding rectangles of the child and parent elements
-    const childRect: DOMRect = olRef.current.getBoundingClientRect();
-
-    const newTop: number =
-        Number(olRef.current.style.top.replace("px", "")) + deltaY;
-    const isWithinResetThresholdScrollingDown: boolean =
-        newTop - get25Percent(childRect.height) >= 10;
-    const isWithinResetThresholdScrollingUp: boolean =
-        newTop + get25Percent(childRect.height) <= 10;
-
-    olRef.current.style.top = `${newTop}px`;
-
-    if (deltaY > 0 && isWithinResetThresholdScrollingDown) {
-        olRef.current.style.top = `${
-            newTop - get25Percent(childRect.height)
-        }px`;
-    } else if (deltaY < 0 && isWithinResetThresholdScrollingUp) {
-        olRef.current.style.top = `${
-            newTop + get25Percent(childRect.height)
-        }px`;
-    } else {
-        olRef.current.style.top = `${newTop}px`;
-    }
-}
-
-function onMouseDown(
-    event: React.MouseEvent<HTMLOListElement>,
-    {
-        olRef,
-        startingMousePositionRef,
-        startingOlTop,
-        onMouseMove,
-        onMouseUp,
-    }: {
-        olRef: React.MutableRefObject<HTMLOListElement | null>;
-        startingMousePositionRef: React.MutableRefObject<number | null>;
-        startingOlTop: React.MutableRefObject<number | null>;
-        onMouseMove: (event: MouseEvent) => void;
-        onMouseUp: () => void;
-    }
-) {
-    if (!olRef.current) {
-        return;
-    }
-
-    startingOlTop.current = Number(
-        window.getComputedStyle(olRef.current).top.replace("px", "")
-    );
-    startingMousePositionRef.current = event.clientY;
-
-    document.addEventListener("mousemove", onMouseMove);
-    document.addEventListener("mouseup", onMouseUp);
 }
 
 const LettersDraggable = ({
@@ -165,24 +92,6 @@ const LettersDraggable = ({
 
     function onOriginalOlMouseUp() {
         document.removeEventListener("mousemove", onOriginalOlMouseMove);
-
-        if (!originalOlRef.current) {
-            return;
-        }
-        // const elements = originalOlRef.current.getElementsByTagName("li");
-
-        // console.log(
-        //     "onMouseUp",
-        //     [...elements].map((e) => {
-        //         return {
-        //             letter: e.innerText,
-        //             position: e.getBoundingClientRect().top + window.scrollY,
-        //         };
-        //     }),
-        //     window.innerHeight,
-        //     `Center: ${window.innerHeight / 2}`
-        // );
-
         document.removeEventListener("mouseup", onOriginalOlMouseUp);
     }
 
