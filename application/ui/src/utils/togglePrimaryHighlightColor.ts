b56185f914465760b92e192947821c931aa32d38
theodoremoreland <theodoremoreland@yahoo.com>
2024-12-24 02:36:31 -0600

    progress on moving towards black and yellow color theme

--- application/ui/src/utils/togglePrimaryHighlightColor.ts
+++ application/ui/src/utils/togglePrimaryHighlightColor.ts
@@ -2,14 +2,8 @@ export default (isPositiveRotation: boolean): void => {
     const root: HTMLElement = document.documentElement;
 
     if (isPositiveRotation) {
-        root.style.setProperty(
-            "--primary-highlight-color",
-            "var(--tertiary-color)"
-        );
+        root.style.setProperty("--highlight-color", "var(--secondary-color)");
     } else {
-        root.style.setProperty(
-            "--primary-highlight-color",
-            "var(--quaternary-color)"
-        );
+        root.style.setProperty("--highlight-color", "var(--quaternary-color)");
     }
 };
