4f458336d8448adfe895edbdabca921c467b8929
theodoremoreland <theodoremoreland@yahoo.com>
2024-12-26 19:26:03 -0600

    added text to RotInfo modal

--- application/ui/src/components/Modal/RotInfo/RotInfo.tsx
+++ application/ui/src/components/Modal/RotInfo/RotInfo.tsx
@@ -12,7 +12,25 @@ const RotInfo = ({ handleClose }: Props): ReactElement => {
     return (
         <Modal title="Change Rotation" handleClose={handleClose}>
             <div>
-                <article></article>
+                <article>
+                    <h3>How to change the rotation</h3>
+                    <p>
+                        To change the rotation (rot), use the columns in the
+                        middle of the page. The columns can be dragged
+                        vertically to change the rot.
+                        <br />
+                        <br />
+                        The left column is the original language, and the right
+                        column is the rotated language. The rot will be applied
+                        to the original language and output in the rotated
+                        language.
+                        <br />
+                        <br />
+                        The rot can be positive or negative, but is positive by
+                        default. Double click on one of the columns to toggle
+                        between positive and negative rotations.
+                    </p>
+                </article>
             </div>
         </Modal>
     );
